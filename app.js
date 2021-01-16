4b2b935005ec8139667524b2e65c9b811a4f38d6
spartan10234 <eduardo10234@live.com>
2021-01-16 14:45:33 -0800

    Testing html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res)=> {
-    res.send(<h1>Hello World</h1>);
+    res.send("<h1>Hello World</h1>");
 });
 
 app.listen(PORT, () => {
