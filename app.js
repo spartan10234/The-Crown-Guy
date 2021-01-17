e2ae44c188cadd98a0339783a980eb66f7a50712
spartan10234 <eduardo10234@live.com>
2021-01-17 14:54:15 -0800

    html content working

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const app = require("express")();
+const express = require('express') 
 const PORT = process.env.PORT || 3000;
+const app = express() 
 
-app.get("/", (req, res)=> {
-    res.sendFile(path.join(__dirname, 'Public', 'index.html'));//send("Hello World");
+app.get('/', function(request, response){
+    response.sendFile(__dirname +'/Public/index.html');
 });
 
 app.listen(PORT, () => {
     console.log(`App up at port ${PORT}`);
 });
+
+/*const app = require("express")();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname+"/Public/index.html")); //send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`);
+});
+*/
